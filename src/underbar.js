069af600f714758dfe599708d58d9845cdd3294e
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-10-04 00:56:08 -0700

    Write _.reject using _.filter

--- src/underbar.js
+++ src/underbar.js
@@ -93,6 +93,17 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    let input = collection.slice(0);
+    let trueArray = _.filter(collection, test);
+    for(let i = 0; i < trueArray.length; i++){
+      for(let j = 0; j < input.length; j++){
+        if(input[j] === trueArray[i]){
+          input.splice(j,1);
+          j--;
+        }
+      }
+    }
+    return input;
   };
 
   // Produce a duplicate-free version of the array.
